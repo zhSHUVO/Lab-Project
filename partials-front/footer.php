a052d2000bd28b10f4d9158a5c2cb45dd5e6cdef
mdRedoyrRajoan <rajoanxalpah129@gmail.com>
2021-12-22 17:19:42 +0600

    partials-front

---
+++ partials-front/footer.php
@@ -0,0 +1,29 @@
+<!-- social Section Starts Here -->
+<section class="social cont">
+    <div class="container text-center ">
+        <ul>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
+            </li>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+            </li>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
+            </li>
+        </ul>
+    </div>
+</section>
+<!-- social Section Ends Here -->
+
+<!-- footer Section Starts Here -->
+<section class="footer  navbar cont">
+    <div class="container text-center text-white navbar ">
+        <p>&#169; 2021 All rights reserved to Ghost Nation.</p>
+    </div>
+</section>
+<!-- footer Section Ends Here -->
+
+</body>
+
+</html>
